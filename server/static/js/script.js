1a1c509fdd43fdbe99bc5f9fec2c1d06e8dfa248
Bogdan Matsyakh <bmatsiakh@gmail.com>
2016-03-23 20:28:10 +0200

    Refactor

--- server/static/js/script.js
+++ server/static/js/script.js
@@ -2,15 +2,9 @@ window.onload = function() {
 	var changeContentButton = document.getElementById('send-request');
 
 	changeContentButton.addEventListener('click', function(event) {
-		var xhttp = new XMLHttpRequest();
-		xhttp.onreadystatechange = function() {
-			console.log(xhttp.readyState, xhttp.status);
-			if (xhttp.readyState == 4 && xhttp.status == 200) {
-				// success code
-			}
-		};
-		xhttp.open("GET", "data.json", true);
-		xhttp.send();
+		xhr.get('data/data.json', function(response) {
+			console.log(response);
+		});
 	});
 
 
