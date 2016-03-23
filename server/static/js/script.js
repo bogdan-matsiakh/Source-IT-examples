6c0bedf43b96751c3fabe0318b00933efca35840
Bogdan Matsyakh <bmatsiakh@gmail.com>
2016-03-23 21:04:51 +0200

    Add NASA API handling

--- server/static/js/script.js
+++ server/static/js/script.js
@@ -1,5 +1,7 @@
 window.onload = function() {
 	var changeContentButton = document.getElementById('send-request');
+	var showMarsImagesButton = document.getElementById('show-mars-images');
+	var marsImages = document.getElementById('mars-images');
 
 	changeContentButton.addEventListener('click', function(event) {
 		xhr.get('data/data.json', function(response) {
@@ -7,6 +9,10 @@ window.onload = function() {
 		});
 	});
 
-
+	showMarsImagesButton.addEventListener('click', function(event) {
+		xhr.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=CM1T7kQolcZFhhewvRLrI2QRikGKizQ0AFp7PLGo', function(response) {
+			console.log(JSON.parse(response));
+		});
+	});
 }
 
